cb45867e735f3d8cbe45529e355eff35085b6086
KICKER <gksdbfla12@naver.com>
2021-10-18 18:14:19 +0900

    [add]json파일생성 코드 추가

--- google_api_test.py
+++ google_api_test.py
@@ -1,11 +1,17 @@
 #sample wav file location: F:\pro\pbl-sample
+from base64 import encode
 
-def transcribe_gcs_with_word_time_offsets(speech_file):
+from google.cloud import speech
+import numpy as np
+import io
+def transcribe_with_word_time_offsets(speech_file):
     """Transcribe the given audio file asynchronously and output the word time
     offsets."""
     from google.cloud import speech
     import numpy as np
     import io
+    import json
+    from collections import OrderedDict
 
     # Instantiates a client
     client = speech.SpeechClient()
@@ -24,14 +30,19 @@ def transcribe_gcs_with_word_time_offsets(speech_file):
     operation = client.long_running_recognize(config=config, audio=audio)
 
     print("Waiting for operation to complete...")
-    result = operation.result(timeout=90)
+    result = operation.result(timeout=180)
 
     for result in result.results:
         alternative = result.alternatives[0]
         print("Transcript: {}".format(alternative.transcript))
         print("Confidence: {}".format(alternative.confidence))
 
+        data_total_form_list = {
+            "sentence" : []
+        }
+
         for word_info in alternative.words:
+            data_form = OrderedDict()
             word = word_info.word
             start_time = word_info.start_time
             end_time = word_info.end_time
@@ -40,4 +51,18 @@ def transcribe_gcs_with_word_time_offsets(speech_file):
                 f"Word: {word}, start_time: {start_time.total_seconds()}, end_time: {end_time.total_seconds()}"
             )
 
-transcribe_gcs_with_word_time_offsets('F:\pro\pbl-sample\sample2.wav')
+            #word들을 json형태로 바꿔주기
+            #이유:csv변환 코드가 json만 받아서.
+            data_total_form_list["sentence"].append({
+                'word' : word,
+                'start_time' : start_time.total_seconds(),
+                'end_time' : end_time.total_seconds()
+            })
+
+    #json 파일 생성
+    json_data = json.dumps(data_total_form_list)
+    json_decode_data = json_data.encode().decode('unicode-escape')
+    with open('test.json', 'w', encoding='UTF-8') as f:
+        f.write(json_decode_data)
+
+transcribe_with_word_time_offsets('F:\pro\pbl-sample\sample2.wav')
